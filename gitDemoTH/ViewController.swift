123cb1318bb0a6e00ad57512d498a347a03f9a10
asoutomaior <alex.soutomaior@gmail.com>
2016-12-29 08:18:24 +0100

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Alex Souto-Maior on 29.12.16.
+//  Copyright © 2016 Alex Souto-Maior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
